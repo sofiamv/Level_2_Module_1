5729c7635d83bc51e713dd91021f2f3fd6f156db
league <league@wts9.attlocal.net>
2018-06-09 15:56:38 -0700

    Intro to array lists

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,35 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> fruit = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		fruit.add("Banana");
+		fruit.add("kiwi");
+		fruit.add("papaya");
+		fruit.add("Watermelon");
+		fruit.add("raspberry");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = 0; i<= fruit.size()-1 ; i++) {
+			System.out.println(fruit.get(i));
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		for(String s:fruit) {
+			System.out.println(s);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for(int i = 1; i <= fruit.size()-1 ; i+=2) {
+			System.out.println(fruit.get(i));
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for(int h = fruit.size()-1; h >= 0 ; h--) {
+			System.out.println(fruit.get(h));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+	
 	}
 }
